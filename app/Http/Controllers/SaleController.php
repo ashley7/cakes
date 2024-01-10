22f6f3ea8523d0a89a9ad291cf5366f433da3aa1
Ashley7 <ashley7520charles@gmail.com>
2024-01-10 22:02:27 +0300

    Improves the application

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -93,8 +93,6 @@ class SaleController extends Controller
 
         $email = hex2bin($hex_email);
 
-        return $email;
-
         $sale = Sale::where('email',$email)->get();
 
         if(empty($sale)) return "Resource Not found";
