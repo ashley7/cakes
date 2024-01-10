7362c7753f9b530a52b1fcee99a064bf787ffd9c
Ashley7 <ashley7520charles@gmail.com>
2024-01-10 22:10:05 +0300

    Improves the application

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,15 @@ Route::get('/', function () {
         return view('auth.login');
 
     return redirect("home");
-    
+
 });
 
 Auth::routes();
 
-Route::group(['middleware' => ['auth']], function () {
-
 Route::get('your_file/{hex_email}','SaleController@show');
 
+Route::group(['middleware' => ['auth']], function () {
+    
     Route::get('/home', 'HomeController@index')->name('home');
 
     Route::post('sales','SaleController@store');
