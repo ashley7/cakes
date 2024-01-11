7931ef21a2a0d2b2c4767771a09828bfd23ba8bc
Ashley7 <ashley7520charles@gmail.com>
2024-01-11 18:27:42 +0300

    disabling right-click

--- resources/views/sales/accessfile.blade.php
+++ resources/views/sales/accessfile.blade.php
@@ -3,15 +3,49 @@
 @section('content')
 <div class="container">    
     <div class="card">               
-        <div class="card-body"> 
-            
-             <embed src="{{asset('files/'.$sale->file_url)}}#toolbar=0" type="application/pdf" frameborder="0" width="100%" height="1000px" allowfullscreen sandbox="allow-same-origin allow-scripts"></embed>
+        <div class="card-body">            
+            <div id="pdfContainer">
+                 <embed src="{{asset('files/'.$sale->file_url)}}#toolbar=0" type="application/pdf" width="100%" height="1000px" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts"></embed>
 
-                                    
+                 <div id="contextMenuMessage">Right-click is disabled</div>
+            </div>                                    
         </div>
     </div>
 </div>
  
+@endsection
+
+@section('styles')
+
+    <style>
+         body {
+            margin: 0;
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        #pdfContainer {
+            position: relative;
+        }
+
+        #contextMenuMessage {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 10px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            display: none;
+        }
+    </style>
+ 
+
 @endsection
 
 @push('scripts')
@@ -19,7 +53,16 @@
         document.addEventListener('contextmenu', function (e) {
             e.preventDefault();
         });
-    </script>
 
- 
+        const pdfContainer = document.getElementById('pdfContainer');
+        const contextMenuMessage = document.getElementById('contextMenuMessage');
+
+        pdfContainer.addEventListener('contextmenu', function (event) {
+            event.preventDefault();
+            contextMenuMessage.style.display = 'block';
+            setTimeout(() => {
+                contextMenuMessage.style.display = 'none';
+            }, 2000); 
+        });
+    </script> 
 @endpush
