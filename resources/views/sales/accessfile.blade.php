9e58c898767ff2cd74522e63f6f18d247079114e
Ashley7 <ashley7520charles@gmail.com>
2024-01-11 09:02:03 +0300

    Clean ups

--- resources/views/sales/accessfile.blade.php
+++ resources/views/sales/accessfile.blade.php
@@ -5,7 +5,7 @@
     <div class="card">               
         <div class="card-body"> 
             
-             <embed src="{{asset('files/'.$sale->file_url)}}" type="application/pdf" frameborder="0" width="100%" height="1000px" allowfullscreen sandbox="allow-same-origin allow-scripts"></embed>
+             <embed src="{{asset('files/'.$sale->file_url)}}#toolbar=0" type="application/pdf" frameborder="0" width="100%" height="1000px" allowfullscreen sandbox="allow-same-origin allow-scripts"></embed>
 
                                     
         </div>
