af0d47e14b1e8dfaefaf70ca45566cdf6fa97ae8
Ashley7 <ashley7520charles@gmail.com>
2024-01-12 19:22:45 +0300

    Clean ups

--- resources/views/sales/accessfile.blade.php
+++ resources/views/sales/accessfile.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">    
-    <div class="card">               
-        <div class="card-body"> 
-           
-            <p>Desclimer: This book is subjected to Copy right and you shall not share or make duplicates of it or re-distribute it.</p>
-
-               
-            <embed src="{{asset('files/'.$sale->file_url)}}#toolbar=0" type="application/pdf" width="100%" height="1000px" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts"></embed>                                          
-        </div>
-    </div>
-</div>
+ <p>Desclimer: This book is subjected to Copy right and you shall not share or make duplicates of it or re-distribute it.</p>
+ <embed src="{{asset('files/'.$sale->file_url)}}#toolbar=0" type="application/pdf" width="100%" frameborder="0" allowfullscreen></embed>                                          
+    
  
 @endsection
 
@@ -52,12 +44,11 @@
 
 <script>
     document.addEventListener('contextmenu', function(event) {
-      var isPdfElement = event.target.nodeName === 'EMBED' && event.target.type === 'application/pdf';
+      var isPdfElement = event.target.nodeName == 'EMBED' && event.target.type === 'application/pdf';
 
       if (isPdfElement) {
         event.preventDefault();
-        // alert('Right-click is disabled on this PDF.');
-      }
+       }
     });
   </script>
 @endpush
